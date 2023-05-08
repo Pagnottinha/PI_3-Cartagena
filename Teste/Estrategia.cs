2e969428599cc0b4a5790615e1a3b37c182397a6
Gustavo Pagnotta <gustavopagfaria@gmail.com>
2023-05-08 18:10:07 -0300

    Começo da estratégia

---
+++ Teste/Estrategia.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teste
+{
+    abstract internal class Estrategia
+    {
+        protected Dictionary<int, Casa> tabuleiro;
+        protected Jogador Jogador;
+
+        public Estrategia(Dictionary<int, Casa> tabuleiro, Jogador Jogador)
+        {
+            this.tabuleiro = tabuleiro;
+            this.Jogador = Jogador;
+        }
+
+        public abstract void jogadaAutomatica();
+    }
+}
---
+++ Teste/EstrategiaInicial.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teste
+{
+    internal class EstrategiaInicial : Estrategia
+    {
+        public EstrategiaInicial(Dictionary<int, Casa> tabuleiro, Jogador Jogador)
+            : base(tabuleiro, Jogador) { }
+
+        public override void jogadaAutomatica()
+        {
+            foreach(Peao peao in Jogador.peoes) {
+                if (voltarComprarDuas(peao))
+                {
+                    Jogador.Jogar(peao.posicao, tabuleiro);
+                    return;
+                }
+
+
+            }
+        }
+
+        bool voltarComprarDuas(Peao peao)
+        {
+            for (int i = peao.posicao; i > 1; i--)
+            {
+                switch(tabuleiro[i].peoes.Count)
+                {
+                    case 1:
+                        return false;
+                    case 2:
+                        return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
