50963823525c19b3ea738ce81f3f43941aaa1843
Pagnottinha <gustavopagfaria@gmail.com>
2023-05-10 22:04:45 -0300

    Timer aumentado para 5s

--- Teste/PartidaForm.Designer.cs
+++ Teste/PartidaForm.Designer.cs
@@ -149,7 +149,7 @@
             // 
             // tmrVerificarVez
             // 
-            this.tmrVerificarVez.Interval = 3000;
+            this.tmrVerificarVez.Interval = 5000;
             this.tmrVerificarVez.Tick += new System.EventHandler(this.tmrVerificarVez_Tick);
             // 
             // lblQtdTricornio
